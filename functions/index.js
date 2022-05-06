9e0391b0c1be12ee94e3ef2533d49933089bcb94
Pablo Hernandez <hpablo575@gmail.com>
2022-05-06 13:30:36 -0600

    finished backend rules and finished functionality for frontend

---
+++ functions/index.js
@@ -0,0 +1,26 @@
+const functions = require("firebase-functions");
+const Filter = require('bad-words');
+
+ const admin = require('firebase-admin');
+ admin.initializeApp();
+
+ const db = admin.firestore();
+
+ exports.detectEvilUsers = functions.firestore
+
+exports.detectEvilUsers = functions.firestore 
+    .document('messages/{msgId}')
+    .onCreate(async (doc, ctx) => {
+        
+        const filter = new Filter();
+        const { text, uid } = doc.data();
+
+        if (filter.isProfane(text)) {
+            
+            const cleaned = filter.clean(text);
+            await doc.ref.update({text: `I got BANNED for like for saying... ${cleaned} `})
+
+            await db.collection('banned').doc(uid).set({});
+        }
+
+    })
